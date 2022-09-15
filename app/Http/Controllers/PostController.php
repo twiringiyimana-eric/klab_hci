ea04d67e30995459df37fb5ca090c7efb4b3766f
Eric <khalifanineza42@gmail.com>
2022-09-15 22:00:48 +0200

    message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,16 +42,16 @@ class PostController extends Controller
         );
          //$imagePath = $request->image->store('/uploads', 'public');
         $file=cloudinary()->uploadFile($request->file('image')->getRealPath())->getSecurePath();
-        $post = $request->user()->posts()->create(
-            [
-                'title' => $request->title,
-                'content' => $request->content,
-               ' image'=> $file
-        ]);
+        // $post = $request->user()->posts()->create(
+        //     [
+        //         'title' => $request->title,
+        //         'content' => $request->content,
+        //        ' image'=> $file
+        // ]);
 
         return response()->json([
             'message' => 'success',
-            'post' => $post
+            'post' => $file
         ], 200);
     }
 
